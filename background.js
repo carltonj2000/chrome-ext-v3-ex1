82b570cb9ea2d699ebc01428cebf759ad924c74c
Carlton Joseph <carlton.joseph@gmail.com>
2022-08-16 16:09:27 -0700

    initial commit

---
+++ background.js
@@ -0,0 +1,35 @@
+console.log("background");
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "notify",
+    title: "Notify!: %s",
+    contexts: ["selection"],
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if ("notify" === info.menuItemId) {
+    notify(info.selectionText);
+  }
+});
+
+chrome.runtime.onMessage.addListener((data) => {
+  if (data.type === "notification") {
+    notify(data.message);
+  }
+});
+
+const notify = (message) => {
+  chrome.storage.local.get(["notifyCount"], (data) => {
+    let value = data.notifyCount || 0;
+    chrome.storage.local.set({ notifyCount: Number(value) + 1 });
+  });
+
+  return chrome.notifications.create("", {
+    type: "basic",
+    title: "Notify!",
+    message: message || "Notify!",
+    iconUrl: "./assets/icons/icon128.png",
+  });
+};
